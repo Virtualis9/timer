b747250a6e05aee36037a8c481d296884ffe05e4
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2024-05-05 19:34:30 +0100

    Basic Stop watch created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect, useRef} from 'react'
+
+interface time {
+  time: number,
+  state: boolean
+}
 
 function App() {
+  const startTimeRef = useRef(0);
+  const intervalIdRef = useRef(null);
+  const [isRunning, setIsRunning] = useState(false);
+  const[ElapsedTime, setElapsedTime] = useState(0);
+  
+  useEffect(() => {
+    if (isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current);
+      }, 10);
+    } else {
+      clearInterval(intervalIdRef.current); 
+    }
+  
+    return () => {
+      clearInterval(intervalIdRef.current); 
+    };
+  }, [isRunning]); 
+
+  const stop = () =>{
+    setIsRunning(false)
+  }
+
+  const start = () =>{
+    setIsRunning(true)
+    startTimeRef.current = Date.now() 
+    
+
+  }
+
+  const restart = () =>{
+    setElapsedTime(0);
+    setIsRunning(false);
+    
+
+  }
+
+  const formatTime = () =>{
+
+    let hours = Math.floor(ElapsedTime / (1000 * 60 * 60))
+    let minutes = Math.floor(ElapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(ElapsedTime / (1000) % 60);
+    let miliseconds = Math.floor(ElapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    miliseconds = String(miliseconds).padStart(2, "0")
+
+    return `${minutes}:${seconds}:${miliseconds}`;
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='timer'>{formatTime()}</div>
+      <div className='controls'>
+        <button onClick={start} className='button-start'>Start</button>
+        <button onClick={restart} className='button-restart'>Restart</button>
+        <button onClick={stop} className='button-stop'>Stop</button>
+      </div>
     </div>
   );
 }
